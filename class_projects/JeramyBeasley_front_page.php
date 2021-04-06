2c34be37040b6bf6cf722c64d57d8dd377e0934f
JeramyBeasley <81545586+JeramyBeasley@users.noreply.github.com>
2021-04-05 21:38:05 -0500

    Add files via upload

---
+++ class_projects/JeramyBeasley_front_page.php
@@ -0,0 +1,35 @@
+<?php
+  /* Jeramy Beasley
+    4/5/2021
+    M02 Lab Assignment - Using Arrays
+  */
+
+
+  $pictures = array('earth.png', 'car-1.png',
+                    'car-2.png');
+
+  shuffle($pictures);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ian's Random Pictures</title>
+  </head>
+  <body>
+    <h1>Ian's Random Pictures</h1>
+      <div align="center">
+      <table style="width: 100%; border: 0">
+        <tr>
+        <?php
+        for ($i = 0; $i < 3; $i++) {
+          echo "<td style=\"width: 33%; text-align: center\">
+                <img src=\"";
+          echo $pictures[$i];
+          echo "\"/></td>";
+        }
+        ?>
+        </tr>
+     </table>
+     </div>
+  </body>
+</html>
